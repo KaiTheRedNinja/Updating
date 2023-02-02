0bca04e5d5361f0efb6d216de191f1fe860b1d5f
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-02-02 19:26:56 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UpdatedSwiftUI
+
+A description of this package.
---
+++ Sources/UpdatedSwiftUI/UpdatedSwiftUI.swift
@@ -0,0 +1,6 @@
+public struct UpdatedSwiftUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
